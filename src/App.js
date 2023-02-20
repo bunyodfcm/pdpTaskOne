551e4b51a973004224ae52aa4f6ead40d9872ef7
bunyodfcm <bunyod5877@gmail.com>
2023-02-20 23:48:43 +0500

    123

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { useState, useEffect } from "react";
+
 function App(props) {
-  return <div>{console.log(props)}
-    <p>hello {props.name}</p>
-  </div>
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log("salom");
+  });
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount((prev) => prev + 1)}>change</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
